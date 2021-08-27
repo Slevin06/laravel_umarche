c9c579205c369d629bd0e950bdcce75ae97cfd83
Yuta Tamai <badyuta06@gmail.com>
2021-08-27 18:05:05 +0900

    名前付きcomponent

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,10 @@
 <x-tests.app>
+
+    {{-- 名前付きslot例 --}}
+    <x-slot name="header">
+        header 1
+    </x-slot>
+
     {{-- コンポーネントの$slotに下記が入る。--}}
     component test 1
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,8 @@
 <x-tests.app>
+
+    <x-slot name="header">
+        header 2
+    </x-slot>
+
     component test 2
 </x-tests.app>

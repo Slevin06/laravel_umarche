86f763e9f89f9e2c82b5a2b50612098119b45bbe
Yuta Tamai <badyuta06@gmail.com>
2021-09-11 23:19:15 +0900

    マルチログイン

---
+++ app/Models/Owner.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+/**
+ * 自作
+ * 認証に使うためにUsersモデルを参考にextends Authenticatable
+ *
+ * Class Owner
+ * @package App\Models
+ */
+class Owner extends Authenticatable
+{
+    /**
+     * 下記もUserモデルの内容ベース
+     */
+
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}

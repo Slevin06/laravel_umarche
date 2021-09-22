5b1156be7b9c8d3a9feaeabeb78c931d53fdf4a1
Yuta Tamai <badyuta06@gmail.com>
2021-09-22 11:43:56 +0900

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Owner;
+use Carbon\Carbon;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class OwnersController extends Controller
 {
@@ -18,17 +25,33 @@ class OwnersController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View|Response
      */
-    public function index(): \Illuminate\Http\Response
+    public function index()
     {
-        dd('test');
+        // Carbonを使ってみる
+        $dateNow = Carbon::now();
+        $dateParse = Carbon::parse(now());
+        echo $dateNow . "\n";
+        echo $dateParse;
+
+        // データを扱う方法のバリエーションテスト
+        $eloquentAll = Owner::all();
+        $querybuilderGet = DB::table('owners')->select('name', 'created_at')->get();
+//        $querybuilderFirst = DB::table('owners')->select('name')->first();
+//        $collectionTest = collect([
+//            'name' => 'test',
+//        ]);
+//        dd($eloquentAll, $querybuilderGet, $querybuilderFirst, $collectionTest);
+
+        return view('admin.owners.index',
+            compact('eloquentAll', 'querybuilderGet'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -38,8 +61,8 @@ class OwnersController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -49,8 +72,8 @@ class OwnersController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function show($id)
     {
@@ -60,8 +83,8 @@ class OwnersController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function edit($id)
     {
@@ -71,9 +94,9 @@ class OwnersController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -83,8 +106,8 @@ class OwnersController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function destroy($id)
     {
